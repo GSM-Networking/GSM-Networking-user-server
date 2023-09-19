0dae247bd115f561a7e8504f7593236d21502b30
SungGil-5125 <82089918+SungGil-5125@users.noreply.github.com>
2023-09-19 16:59:34 +0900

    :sparkles: :: jpa, spring, test 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,23 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.7.15"
+	id("org.springframework.boot") version "2.7.9"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
+	kotlin("plugin.jpa") version "1.6.21"
+}
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
 }
 
 group = "com.example"
@@ -18,10 +31,37 @@ repositories {
 	mavenCentral()
 }
 
+dependencyManagement {
+	dependencies {
+		dependencySet("io.kotest:5.7.2") {
+			entry("io.kotest:kotest-runner-junit5-jvm")
+			entry("io.kotest:kotest-assertions-core-jvm")
+			entry("io.kotest:kotest-extensions-spring")
+		}
+	}
+}
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	/* spring app */
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+	/** jpa **/
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+	/* DB */
+	implementation("mysql:mysql-connector-java:8.0.32")
+	implementation("org.springframework.boot:spring-boot-starter-data-redis")
+
+	/* test */
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm")
+	testImplementation("io.kotest:kotest-assertions-core")
+	testImplementation("io.kotest:kotest-extensions-spring")
+	testImplementation("io.mockk:mockk:1.12.0")
 }
 
 tasks.withType<KotlinCompile> {
